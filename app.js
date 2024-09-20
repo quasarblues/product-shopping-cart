89925f0059b8957aefe2fa415e7350592d306085
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2024-09-20 23:43:04 +0900

    Added increment and decrement functionality to product cards

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const gridWrapperEl = document.querySelector('.grid-wrapper');
-const productImg = document.querySelector('.product-card img');
 
-
-const decrementBtn = document.querySelector('.decrement');
-const incrementBtn = document.querySelector('.increment');
-const itemQuantityDisplay = document.querySelector('.item-quantity');
-let itemQuantity;
 
 const loadProducts = async () => {
-    try {
-        const res = await fetch('data.json');
-        const data = await res.json();
-        return data;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const res = await fetch('data.json');
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 const createProductCards = (data) => {
-    try {
-        const { image, name, category, price } = data;
-        const productCard = document.createElement('div');
-        productCard.classList.add('product-card');
-        productCard.innerHTML = `
+  try {
+    const { image, name, category, price } = data;
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    productCard.innerHTML = `
         
         <div class="product-image-container">
             <picture>
@@ -50,56 +44,53 @@ const createProductCards = (data) => {
           <p class="price">$${price.toFixed(2)}</p>
         </div>
         `
-        gridWrapperEl.append(productCard);
+    gridWrapperEl.append(productCard);
 
-        const addToCartBtns = productCard.querySelectorAll('.add-to-cart');
-        const adjustQuantityDivs = productCard.querySelectorAll('.adjust-quantity')
+    const addToCartBtn = productCard.querySelector('.add-to-cart');
+    const adjustQuantityDiv = productCard.querySelector('.adjust-quantity');
+    const productImg = productCard.querySelector('.product-card img');
+    const decrementBtn = productCard.querySelector('.decrement');
+    const incrementBtn = productCard.querySelector('.increment');
+    const itemQuantityDisplay = productCard.querySelector('.item-quantity');
+    let itemQuantity;
+
+    addToCartBtn.addEventListener('click', () => {
+      addToCartBtn.style.display = 'none';
+      adjustQuantityDiv.style.display = 'flex';
+      productImg.classList.add('selected-border')
+      itemQuantity = 1;
+      itemQuantityDisplay.innerText = itemQuantity;
+    });
+
+    incrementBtn.addEventListener('click', () => {
+      itemQuantity += 1;
+      itemQuantityDisplay.innerText = itemQuantity;
+      console.log(itemQuantity);
+    });
+
+    decrementBtn.addEventListener('click', () => {
+      if (itemQuantity > 1) {
+        itemQuantity -= 1;
+        itemQuantityDisplay.innerText = itemQuantity;
+      } else {
+        itemQuantity = 0;
+        itemQuantityDisplay.innerText = itemQuantity;
+        productImg.classList.remove('selected-border');
+        adjustQuantityDiv.style.display = 'none';
+        addToCartBtn.style.display = 'flex';
+      }
+      console.log(itemQuantity);
+    })
 
 
-        addToCartBtns.forEach(addBtn => {
-            addBtn.addEventListener('click', () => {
-                addBtn.style.display = 'none';
-
-                adjustQuantityDivs.forEach(adjustDiv => {
-                    adjustDiv.style.display = 'flex';
-                })
-            });
-        })
-
-    } catch (err) {
-        console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const products = await loadProducts();
-    products.forEach(product => {
-        createProductCards(product);
-    });
+  const products = await loadProducts();
+  products.forEach(product => {
+    createProductCards(product);
+  });
 });
-
-// addToCartBtnHard.addEventListener('click', () => {
-//     addToCartBtn.style.display = 'none';
-//     adjustQuantityBtn.style.display = 'flex';
-//     productImg.classList.add('selected-border');
-//     itemQuantity = 1;
-//     itemQuantityDisplay.innerText = itemQuantity;
-// });
-
-// incrementBtn.addEventListener('click', () => {
-//     itemQuantity += 1;
-//     itemQuantityDisplay.innerText = itemQuantity;
-// })
-
-// decrementBtn.addEventListener('click', () => {
-//     if (itemQuantity > + 1) {
-//         itemQuantity -= 1;
-//         itemQuantityDisplay.innerText = itemQuantity;
-//     } else {
-//         addToCartBtn.style.display = 'flex';
-//         adjustQuantityBtn.style.display = 'none';
-//         productImg.classList.remove('selected-border');
-//         itemQuantity = 0;
-//         itemQuantityDisplay.innerText = itemQuantity;
-//     }
-// })
